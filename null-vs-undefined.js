c8a1a16cac2bd54b76b5766bf9e7c46bd26be45c
Rakib021 <rakibul02107089@gmail.com>
2021-02-02 17:50:08 +0600

    undefined vs null

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+let pakhi;
+function add(num1,num2){
+    console.log(num1 + num2);
+    return;
+}
+
+function add (num1,num2){
+    console.log(num1,num2);
+}
+const premik = add(12);
+console.log(result);
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,4,6];
+console.log(ages[11]);
